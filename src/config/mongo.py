4d6c67389ed02e3e2c49e475c3b84ec9940fa7c0
Davit Loem <davitloem25@gmail.com>
2026-05-25 10:14:41 +0700

    Update code for host

--- src/config/mongo.py
+++ src/config/mongo.py
@@ -1,16 +1,35 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+# MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+# MONGO_HOST = os.getenv("MONGO_HOST")
+# MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+
+# url = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/Jobber_City?authSource=admin"
+# client = MongoClient(url)
+
+# def collections(name):
+#     return client["Jobber_City_data"][name]
+
+
 import os
+from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_USERNAME = os.getenv("MONGO_USERNAME")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_HOST = os.getenv("MONGO_HOST")
-MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+# Read the public connection string directly
+mongo_url = os.getenv("MONGO_PUBLIC_URL")
 
-url = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/Jobber_City?authSource=admin"
-client = MongoClient(url)
+if not mongo_url:
+    raise ValueError("MONGO_PUBLIC_URL is not set in the environment variables")
+
+# Pass the full string directly to MongoClient
+client = MongoClient(mongo_url)
 
 def collections(name):
     return client["Jobber_City_data"][name]
