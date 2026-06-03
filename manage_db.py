7b2ec0d064a6f9f3dd8c14e90ef23f224c333935
Arofath <suty2689@gmail.com>
2026-06-03 08:37:44 +0700

    add category file

--- manage_db.py
+++ manage_db.py
@@ -49,6 +49,18 @@ async def seed_database():
                 "created_at": now,
                 "updated_at": now
             },
+            {
+                "name": "Super Admin",
+                "email": "vitloem@gmail.com",
+                "password_hash": default_password,
+                "auth_provider": "local",
+                "role": "admin",
+                "is_active": True,
+                "is_profile_completed": True,
+                "verified_at": now,
+                "created_at": now,
+                "updated_at": now
+            },
         ]
 
         # បញ្ចូលទិន្នន័យទាំងអស់ទៅក្នុង Database ក្នុងពេលតែមួយ
