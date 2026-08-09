2802b58110f30f662d75d1952cdb31e1d18076a7
Sles Rofath <suty2689@gmail.com>
2026-08-10 00:35:57 +0700

    fix route

--- src/domains/employer/applicant/routes/applicant_router.py
+++ src/domains/employer/applicant/routes/applicant_router.py
@@ -34,7 +34,7 @@ async def get_job_applicants_route(
         data=result
     )
     
-@router.get("/jobs/dropdown", response_model=APIResponse)
+@router.get("/dropdown", response_model=APIResponse)
 async def get_job_filter_dropdown_route(
     current_user: dict = Depends(require_employer)
 ):
